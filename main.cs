d29a4b046453be98949e1cd0662ddd89e20470a5
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-10-20 22:45:00 +0000

    Main upload

--- main.cs
+++ main.cs
@@ -1,7 +1,45 @@
 using System;
+using System.Linq;  // Add this line
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    static void Main()
+    {
+        string[] salespeopleNames = { "Danielle", "Edward", "Francis" };
+        char[] salespeopleInitials = { 'D', 'E', 'F' };
+        decimal[] sales = new decimal[3];
+
+        while (true)
+        {
+            Console.WriteLine("Enter salesperson initial (D, E, F) or Z to exit:");
+            char initial = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if (initial == 'Z')
+            {
+                break;
+            }
+
+            int index = Array.IndexOf(salespeopleInitials, initial);
+            if (index == -1)
+            {
+                Console.WriteLine("Error, invalid salesperson selected, please try again");
+                continue;
+            }
+
+            Console.WriteLine($"Enter sale amount for {salespeopleNames[index]}:");
+            decimal sale = decimal.Parse(Console.ReadLine());
+            sales[index] += sale;
+        }
+
+        decimal grandTotal = 0;
+        foreach (var sale in sales)
+        {
+            grandTotal += sale;
+        }
+        Console.WriteLine($"Grand Total: ${grandTotal:0,0.00}");
+
+        int highestSalespersonIndex = Array.IndexOf(sales, sales.Max());  // Use the Max method from Linq
+        Console.WriteLine($"Highest Sale: {salespeopleInitials[highestSalespersonIndex]}");
+    }
 }
